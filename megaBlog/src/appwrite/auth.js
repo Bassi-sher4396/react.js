884a5a1dd884d806b632658767b5a5e07de412fe
Bassi-sher4396 <hardikbassi230@gmail.com>
2025-03-20 18:13:36 +0000

    authentication services

---
+++ megaBlog/src/appwrite/auth.js
@@ -0,0 +1,60 @@
+import conf from '../conf.js'
+
+import { Client, Account, ID } from "appwrite"
+
+export class AuthService {
+    client = new Client()
+    account;
+    constructor(){
+        this.client
+        .setEndpoint(conf.appwriteUrl)
+        .setProject(conf.appwriteProjectId);
+        this.account = new Account(this.client)
+    }
+async createAccount({email,password,name}){
+    try{
+const userAccount = await this.account.create(ID.unique(),email,password,name)
+if(userAccount){
+    //call another method like succesfully creqted account like login
+    return this.login({email,password})
+}
+else{
+    return  userAccount
+}
+    }
+    catch(error){
+        throw error
+    }
+}
+async login({email,password}){
+    try{
+ return await this.account.createEmailPasswordSession(email,password)
+    }
+    catch(error){
+        throw error
+    }
+}
+async getCurrUser(){
+    try{
+return await this.account.get()
+    }
+    catch(error){
+        throw error
+    }
+    return null //agar koi account hoga hi nhi to null react ho jaega cattch tab kaam karega jab services reach hi nhi ho pa rhi 
+}
+async logout(){
+    try{
+        // return await this.account.deleteSession('current')
+        return await this.account.deleteSessions()
+    }
+    catch(error){
+        throw error
+    }
+}
+}
+ 
+const authService = new AuthService()
+
+export default authService 
+
